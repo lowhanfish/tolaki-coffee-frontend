1a176e66f8eba4ff778ea0dc143c30147ed6d20e
lowhanfish <lowhanfish@gmail.com>
2026-08-04 20:55:18 +0800

    added product

---
+++ src/app/(public)/product/components/ContentProduct.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BsChevronRight } from "react-icons/bs";
+
+const ContentProduct = () => {
+    return (
+        <div>
+            <div>
+                <button className='btn-dropdown'>
+                    <p className='flex-1'>Terpoluer</p>
+                    <BsChevronRight className='w-5' />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ContentProduct
--- src/components/HeaderPage.tsx
+++ src/components/HeaderPage.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 const HeaderPage = () => {
     return (
-        <div className='relative w-full h-64 md:h-137 flex items-center justify-center'>
+        <div className='relative w-full h-64 md:h-109 flex items-center justify-center'>
             <div className='z-2 flex flex-col items-center justify-center'>
                 <p className='text-[45px]'>Anoa Coffee</p>
                 <p className='-mt-2'>Pelajari tentang kami</p>
             </div>
             <Image
                 alt='Header Page'
-                src={`/images/header_page.webp`}
+                src={`/images/header_product1.webp`}
                 fill
                 className='object-cover'
             />
