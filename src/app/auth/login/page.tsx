d82107b726c52dae35b04f68233ed638d077d87d
lowhanfish <lowhanfish@gmail.com>
2026-08-18 22:47:31 +0800

    added test login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
     return (
         <div className='bg h-full'>
             <h1>Login</h1>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur accusamus dignissimos autem maxime blanditiis quibusdam quae sit laborum nesciunt, aut at expedita, voluptatem esse qui asperiores unde ducimus a deserunt!
         </div>
     )
 }
